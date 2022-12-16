e3983294f9cd047a3f3203680b33038bc2a34ad4
Filippo <boscofil14@gmail.com>
2022-12-16 17:58:10 +0100

    Facebook Button Login

---
+++ src/components/initFacebookSDK.js
@@ -0,0 +1,29 @@
+const FACEBOOK_APP_ID = "643639507384543";
+
+export default function initFacebookSDK() {
+  return new Promise(resolve => {
+    // Wait for the Facebook SDK to initialize before starting the React app.
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: FACEBOOK_APP_ID,
+        cookie: true,
+        xfbml: true,
+        version: "v15.0",
+      });
+      resolve();
+    };
+
+    // Load the Facebook SDK script.
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  });
+}
