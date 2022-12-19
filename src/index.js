85756f458a274070da56de520784799ea73fb94e
Filippo <boscofil14@gmail.com>
2022-12-19 09:36:14 +0100

    react-router-dom added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import initFacebookSDK from "./components/initFacebookSDK";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 initFacebookSDK().then(() => {
-  root.render(<App />);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
 });
