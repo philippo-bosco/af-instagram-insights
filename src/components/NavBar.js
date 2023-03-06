62abb40ef1b83ff97990e9c4d50830dde51420b3
Filippo <boscofil14@gmail.com>
2023-03-06 10:28:48 +0100

    fix navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,10 @@
-export default function Navbar() {
+export default function Navbar({ isAuth }) {
   //TODO: nascondere la navbar quando non si è ancora effettuato il login
-  return (
+  return isAuth ? (
     <div>
       <p>questa è la navbar</p>
     </div>
+  ) : (
+    <div></div>
   );
 }
