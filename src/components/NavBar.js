09f8fcd9d9af9190d364b07195659805fbc3a17f
Filippo <boscofil14@gmail.com>
2023-03-10 12:25:20 +0100

    navbar data fetch raw

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import secureLocalStorage from "react-secure-storage";
+import axios from "axios";
 
 export default function Navigationbar({ isAuth, toggleAuth }) {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
+  const [userData, setUserData] = useState(null); // stato per contenere i dati dell'utente
+  const storedAT = secureLocalStorage.getItem("AT");
+  const storedIgID = secureLocalStorage.getItem("IgID"); //ig-user-id per effettuare le chiamate alla Graph API
+
+  useEffect(() => {
+    async function fetchData() {
+      if (storedAT && storedIgID) {
+        const response = await axios.get(
+          `https://graph.facebook.com/v13.0/${storedIgID}?fields=followers_count,follows_count,media_count,name,profile_picture_url,username&access_token=${storedAT}`
+        );
+        setUserData(response.data);
+      }
+    }
+    fetchData();
+  }, [storedAT, storedIgID]);
 
   async function handleLogout() {
     setIsLoading(true);
@@ -51,6 +67,27 @@ export default function Navigationbar({ isAuth, toggleAuth }) {
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
+          {userData && (
+            <li className="nav-item">
+              <div className="d-flex align-items-center">
+                <img
+                  src={userData.profile_picture_url}
+                  alt="profile"
+                  width="40"
+                  height="40"
+                  className="rounded-circle mr-2"
+                />
+                <div>
+                  <div>{userData.username}</div>
+                  <div className="small text-muted">
+                    {userData.followers_count} followers |{" "}
+                    {userData.follows_count} following | {userData.media_count}{" "}
+                    posts
+                  </div>
+                </div>
+              </div>
+            </li>
+          )}
           {isAuth && (
             <li className="nav-item">
               <button
--- src/pages/HomeAccount.jsx
+++ src/pages/HomeAccount.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import secureLocalStorage from "react-secure-storage";
 //import LoadInstagramAccount from "../components/AccountLoad";
